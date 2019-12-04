b77ce30ac3cc0f944c626876dcf40b18a69970f4
ed <ed@elirandedi.com>
2019-12-04 19:54:33 +0000

    fns: add newdata() function

--- fns.h
+++ fns.h
@@ -2,14 +2,15 @@ typedef	void	Thread(void*);
 typedef	int	Cmp(void*,void*);	/* shut up kencc */
 typedef	int	Dircmp(Dir*,Dir*);
 
-void	select(void);
+void	select(void);		/* regular functions */
 void	redraw(void);
 void	drawbottom(char*);
+void	newdata(void);
 
-Thread	mousethread;
+Thread	mousethread;		/* thread functions */
 Thread	resizethread;
 Thread	kbdthread;
 Thread	drawthread;
 
-Dircmp	dirstrlencmp;
+Dircmp	dirstrlencmp;		/* for qsort */
 Dircmp	dirnamecmp;
